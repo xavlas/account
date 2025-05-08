507a3ee23d7141ae928b7b5918894e10aa7fae0c
xavlas <lassus.xavier@gmail.com>
2025-05-08 12:06:23 +0200

    init

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.write("Hello world")
+
+import streamlit as st
+
+if not st.user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.user.name}!")
